45bd2e689a092a281b129b5e6e730ec400563cf4
Raul Mayobre <raulma486hotmail.com>
2021-05-07 04:42:09 +0000

    version final calculadora

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-
-    var principal = document.getElementById("principal").value;
-
-    var rate = document.getElementById("rate").value;
-
-    var years = document.getElementById("years").value;
-
-    var interest = principal * years * rate /100;
-
-      var year = new Date().getFullYear()+parseInt(years);
     
+    if (p >0){
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerText="\n If you deposit "+p+",\nat an interest rate of "+rate+"%.\n"+"You will receive an amount of "+interest+",\n in the year "+year
+        } else {
+            alert("Enter a positive number")
+        }
 }
-
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
+    
+
 }
         
